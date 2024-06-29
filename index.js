3453954c5af370ccd1e9c8ab8719879c8a54cd96
Stack Soumik <sarkarsoumik215@gmail.com>
2024-06-30 03:44:45 +0600

    soket done

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import { createServer } from 'node:http';
 import path from 'node:path';
 import { Server } from 'socket.io';
+import os from 'os';
 
 // connection to the public
 const app = express();
@@ -9,10 +10,17 @@ app.use(express.static(path.resolve("./public")));
 
 const server = createServer(app);
 const io = new Server(server);
+
+
 // create connection 
 io.on("connection", (socket) => {
     console.log(" A new user connection", socket.id);
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+        
+    });
 });
+
 
 
 app.get('/', (req, res) => {
